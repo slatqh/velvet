5849364a53a2736f020bf3aac06dd3202f611e9a
Miguel Guerrero <seetalkit@gmail.com>
2019-06-06 15:55:51 -0400

    Fix case of paths

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import Authreducer from '../screens/Auth/Authreducer';
 import Homereducer from '../screens/Home/reducer';
-import ArticleReducer from '../screens/ArticleScreen/articlereducer';
+import ArticleReducer from '../screens/ArticleScreen/ArticleReducer';
 
 export default combineReducers({
   Auth: Authreducer,
