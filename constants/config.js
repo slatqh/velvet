caa816424a9e740925b89bb4254586c47c62c00b
slatqh <32040388+slatqh@users.noreply.github.com>
2019-07-22 20:36:19 -0400

    Delete config.js

--- constants/config.js
+++
@@ -1,6 +0,0 @@
-export const keys = {
-  API_URL: 'http://velvetbackend-env.apf29frtzh.us-east-2.elasticbeanstalk.com',
-  // LOCALHOST: 'http://10.0.2.2:3000',
-  LOCALHOST:
-    'http://velvetbackend-env.apf29frtzh.us-east-2.elasticbeanstalk.com',
-};
